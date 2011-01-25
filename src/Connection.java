e602e5b1e4636ad87f1e62e13409453a98a27bfd
moritz <moritz.winter@gmail.com>
2011-01-25 10:56:59 +0100

    Terminal.java removed

--- src/Connection.java
+++ src/Connection.java
@@ -8,25 +8,19 @@ public class Connection {
     public Connection() {
 
     }
-    public String getSite(String url) {
+
+    public String getSite(String url) throws MalformedURLException, IOException {
 	String code = "";
 	InputStream is = null;
-	try {
-	    URL url2 = new URL(url);
-	    is = url2.openStream();
-	    code = new Scanner(is).useDelimiter("\\Z").next();
-	} catch (MalformedURLException e) {
-	    code = "Falsche Eingabe";
-	} catch (IOException e) {
 
-	} finally {
-	    if (is != null)
-		try {
-		    is.close();
-		} catch (IOException e) {
-		    
-		}
+	URL url2 = new URL(url);
+	is = url2.openStream();
+	code = new Scanner(is).useDelimiter("\\Z").next();
+
+	if (is != null) {
+	    is.close();
 	}
+
 	return code;
     }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,23 +1,49 @@
-
+import java.io.*;
+import java.net.MalformedURLException;
 
 public class UI {
     private static Connection con;
-    
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
 	boolean running = true;
 	con = new Connection();
 	String input = "";
-	
+
 	while (running) {
 	    System.out.println("URL eingeben:");
-	    input = Terminal.ask(">");
+	    input = ask(">");
 	    if (input.equals("exit")) {
 		running = false;
 	    } else {
-	System.out.println(con.getSite(input));
+		try {
+		    System.out.println(con.getSite("http://" + input));
+		} catch (MalformedURLException e) {
+		    System.out.println("Falsche Eingabe");
+		} catch (IOException e) {
+		    System.out.println("Verbindungsfehler");
+		}
 	    }
 	}
 
-	
     }
+    
+    public static String askString(String prompt) {
+        System.out.print(prompt);
+        return readString();
+    }
+    
+    public static String readString() {
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+    
+    public static String ask(String prompt) {
+        return askString(prompt);
+    }
+    
+    
 }
