dbcc1c0898184af75fef4bbc0bd235dbccc341b4
moritz <moritz.winter@gmail.com>
2011-01-25 17:10:08 +0100

    Added new class FritzBox

---
+++ src/FritzBox.java
@@ -0,0 +1,32 @@
+/**
+ * The class {@code FritzBox} represents a Fritz!Box.
+ * 
+ * @author moritz
+ * 
+ */
+public class FritzBox {
+
+    /**
+     * Tries to wake up the device with mac-address mac.
+     * 
+     * @param mac
+     *            the specified mac-address
+     * @return returns true if the operation succeeded
+     */
+    public boolean wakeUp(String mac) {
+	boolean success = false;
+
+	return success;
+    }
+
+    /**
+     * Tries to restart the Fritz!Box
+     * 
+     * @return returns true if the operation succeeded
+     */
+    public boolean restart() {
+	boolean success = false;
+
+	return success;
+    }
+}
